5e5b03d5bd434aa852261ba9ee0d36293824ed3d
Sean Marquez <capsulecorplab@gmail.com>
2024-01-06 11:52:23 -0700

    purge hugo content

--- serve.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-docker run --rm --volume $PWD:/src -p 1313:1313 -w "/src" capsulecorplab/hugo-asciidoctor-plantuml bash -c "hugo serve --bind=0.0.0.0 --buildDrafts --disableFastRender --destination public"
